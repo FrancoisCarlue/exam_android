746d189656deb9f73dfe3d9c2a214013a804d176
Francois Carlue <francois.carlue@centrale-marseille.fr>
2018-11-08 15:01:44 +0100

    Added Destination class

--- app/src/main/java/com/example/francois/exam_android/Destination.java
+++ app/src/main/java/com/example/francois/exam_android/Destination.java
@@ -1,4 +1,14 @@
 package com.example.francois.exam_android;
 
 public class Destination {
+
+String type;
+String title;
+int distance;
+
+public Destination(String type,String title){
+    this.type = type;
+    this.title = title;
+}
+
 }

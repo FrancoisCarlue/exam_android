cd08a898605ce606c6d3f64f32dea6fc454afed5
Francois Carlue <francois.carlue@centrale-marseille.fr>
2018-11-08 15:30:45 +0100

    Added destination item layout

---
+++ app/src/main/java/com/example/francois/exam_android/DestinationAdapter.java
@@ -0,0 +1,34 @@
+package com.example.francois.exam_android;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+public class DestinationAdapter extends BaseAdapter {
+
+    public class ViewHolder{
+        TextView tvTitre;
+        TextView tvAuteur;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
+}

81f7135498d863356515ea384f1c9b7b9373b94d
Francois Carlue <francois.carlue@centrale-marseille.fr>
2018-11-11 17:03:03 +0100

    Added click listener for restaurant : webview

--- app/src/main/java/com/example/francois/exam_android/MainActivity.java
+++ app/src/main/java/com/example/francois/exam_android/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.francois.exam_android;
 
 import android.app.ListActivity;
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -15,6 +16,7 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -49,6 +51,7 @@ public class MainActivity extends ListActivity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 switch (maBibliotheque.get(position).getType()){
+
                     case "POI":
                         Log.i(TAG, "POI reconnu dans case");
                         Intent intentPOI = new Intent(MainActivity.this,LoadingScreenActivity.class);
@@ -56,6 +59,21 @@ public class MainActivity extends ListActivity {
                         intentPOI.putExtra("id",maBibliotheque.get(position).getId());
                         Log.i(TAG, "putExtra fait");
                         startActivity(intentPOI);
+                        Log.i(TAG, "startactivity lancé");
+                        break;
+
+                    case "RESTAURANT":
+                        Log.i(TAG, "RESTAURANT reconnu dans case");
+                        Intent intentRESTAURANT = null;
+                        try {
+                            intentRESTAURANT = new Intent(Intent.ACTION_VIEW,Uri.parse(jsonTransfere.getJSONObject(position).getString("web")));
+                            intentRESTAURANT.putExtra("id",maBibliotheque.get(position).getId());
+                            Log.i(TAG, "Intent créé");
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                        Log.i(TAG, "putExtra fait");
+                        startActivity(intentRESTAURANT);
                         Log.i(TAG, "startactivity lancé");
                         break;
                 }
